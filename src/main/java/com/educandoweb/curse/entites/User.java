06e1128e83adc90b4572224d4635dee67f9d68a1
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-04-15 09:02:03 -0300

    git and resource

---
+++ src/main/java/com/educandoweb/curse/entites/User.java
@@ -0,0 +1,73 @@
+package com.educandoweb.curse.entites;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String passward;
+
+    public User(){
+    }
+
+    public User(Long id, String name, String email, String phone, String passward) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.passward = passward;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassward() {
+        return passward;
+    }
+
+    public void setPassward(String passward) {
+        this.passward = passward;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
